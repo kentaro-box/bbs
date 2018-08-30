e200cb0c34ca63feb3c1829b7e93cda10aa7d165
kentaro <>
2018-08-30 19:58:00 +0900

    no message

--- write.php
+++ write.php
@@ -1,11 +1,12 @@
 <?php  
+session_start();
 
-if (isset($_POST['name']) && isset($_POST['title']) && isset($_POST['body'])){
+if (isset($_POST['title']) && isset($_POST['body'])){
 
 $name = $_POST['name'];
 $title = $_POST['title'];
 $body = $_POST['body'];
-
+$user_id = $_SESSION['user_id'];
 	$dsn = 'mysql:host=localhost;dbname=bbs;charset=utf8';
 	$user = 'root';
 	$pass = 'root';
@@ -14,12 +15,13 @@ $body = $_POST['body'];
 		$db = new PDO($dsn, $user, $pass);
 		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$stmt = $db->prepare("
-			INSERT INTO posts (title, body, date)
-			VALUES (:title, :body, now())"
+			INSERT INTO posts (title, body, date,user_id)
+			VALUES (:title, :body, now(), :user_id)"
 		);
 
 		$stmt->bindParam(':title', $title, PDO::PARAM_STR);
 		$stmt->bindParam(':body', $body, PDO::PARAM_STR);
+		$stmt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
 		$stmt->execute();
 
 		header('Location: index.php');
