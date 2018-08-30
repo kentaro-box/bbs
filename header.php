badcdc9c3945d70b7144f9310a77e4413a78705b
kentaro <>
2018-08-30 19:56:53 +0900

    ヘッダー<body>まで含めた

--- header.php
+++ header.php
@@ -1,8 +1,14 @@
 <?php 
 
  ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>投稿内容一覧ページ</title>
+ </head>
+ <body>
 <header>
- 		<p><a href="#">掲示板</a></p>
+ 		<p><a href="">掲示板</a></p>
  		<ul>
  			<li><a href="login.php">ログイン</a></li>
  			<li><a href="logout.php">ログアウト</a></li>
