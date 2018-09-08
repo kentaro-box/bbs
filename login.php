fe9e1be608996118bff7f9e09b99ce5f9ff0eeec
kentaro <>
2018-09-08 17:11:21 +0900

    パスワード暗号化終了

--- login.php
+++ login.php
@@ -4,6 +4,8 @@ error_reporting(E_ALL);
 
 session_start();
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 	if (filter_input(INPUT_POST, 'login-name')){
@@ -28,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 
 	if (filter_input(INPUT_POST, 'passwords')){
-		$passwords = $_POST['passwords'];
+		$passwords = password_hash($_POST['passwords'], PASSWORD_DEFAULT);
 	}else {
 		if (filter_input(INPUT_POST, 'logname') || filter_input(INPUT_POST, 'password')){
 		$error_passwords = "";
@@ -63,6 +65,7 @@ if (filter_input(INPUT_POST, 'login-name') && filter_input(INPUT_POST, 'mail') &
 	$name = filter_input(INPUT_POST, 'login-name');
 	$mail = filter_input(INPUT_POST, 'mail');
 	$passwords = filter_input(INPUT_POST, 'passwords');
+	$hash = password_hash($passwords, PASSWORD_DEFAULT);
 	
 	$dsn = 'mysql:host=localhost;dbname=bbs;charset=utf8';
 	$user = 'root';
@@ -87,9 +90,6 @@ if (filter_input(INPUT_POST, 'login-name') && filter_input(INPUT_POST, 'mail') &
 
 				// header('Location: login.php');
 				// exit();
-			}else {
-
-
 			}
 		}
 		
@@ -97,74 +97,74 @@ if (filter_input(INPUT_POST, 'login-name') && filter_input(INPUT_POST, 'mail') &
 		echo $e->getMessage();
 	}
 
+	if(empty($error)){
 
+		try {
+			$db = new PDO($dsn, $user, $pass);
+			$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+			$stmt = $db->prepare("
+				INSERT  INTO user (name, meil, password, date)
+				VALUES (:name, :meil, :password, now())
+				");
+			
+			$stmt->bindParam(':name',$name, PDO::PARAM_STR);
+			$stmt->bindParam(':meil',$mail,PDO::PARAM_STR);
+			$stmt->bindParam(':password',$hash,PDO::PARAM_STR);
+			$stmt->execute();
+			
+				header('Location: completion.php');
+				exit();
 
-// while ($row = $stmt->fetch()){
-// 			$row['name'] = filter_input(INPUT_POST, 'login-name');
-// 			var_dump($row['name']);
-// 		}
-
-if(empty($error)){
-	
-
-
-
-	try {
-		$db = new PDO($dsn, $user, $pass);
-		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-		$stmt = $db->prepare("
-			INSERT  INTO user (name, meil, password, date)
-			VALUES (:name, :meil, :password, now())
-			");
-		
-		$stmt->bindParam(':name',$name, PDO::PARAM_STR);
-		$stmt->bindParam(':meil',$mail,PDO::PARAM_STR);
-		$stmt->bindParam(':password',$passwords,PDO::PARAM_STR);
-		$stmt->execute();
-
-		
-			header('Location: completion.php');
-			exit();
-		
-		
-		
-
-	} catch (Exception $e) {
-		echo $e->getMessage();
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
 	}
+}
+}
 
-}
-}
-}
 if (isset($_SESSION['user_id'])){
 	header('Location: index.php');
 }else{
 	if (!empty($_POST['logname']) && !empty($_POST['password'])){
 	$loginName = $_POST['logname'];
-	$password = $_POST['password'];
-
+	$post_password = $_POST['password'];
+	
 	$dsn = 'mysql:host=localhost;dbname=bbs;charset=utf8';
 	$user = 'root';
 	$pass = 'root';
+
 	try {
 		$db = new PDO($dsn, $user, $pass);
 		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$stmt = $db->prepare("
-			SELECT * FROM user WHERE name=:name AND password=:password
+			SELECT name,password,user_id FROM user 
 			");
 		$stmt->bindParam(':name',$loginName, PDO::PARAM_STR);
 		$stmt->bindParam(':password',$password,PDO::PARAM_STR);
-
 		$stmt->execute();
+		while($row = $stmt->fetch()){
 
-		if ($row = $stmt->fetch()){
-			$_SESSION['user_id'] = $row['user_id'];
-			header('Location: index.php');
-			exit();
-		} 
+
+		
+		$dbinner_pass = $row['password'];
+
+
+			if (password_verify($_POST['password'], $dbinner_pass)) {
+								  $_SESSION['user_id'] = $row['user_id'];
+								  header('Location: index.php');
+							exit();
+							} else {
+								$unapproved = "名前またはパスワードが違います。";
+							}
+		}
+
+
+						
+		
+	
 	} catch (Exception $e) {
 		echo $e->getMessage();
-	}
+}
 } 
 }
 
@@ -176,24 +176,25 @@ if (isset($_SESSION['user_id'])){
 		<form action="" method="post">
 			<p>お名前<span><?php if(isset($error_logname)){ echo $error_logname;} ?></span></p>
 			<p><input type="text" name="logname" value="<?php if(isset($_POST['logname'])){
-				echo $_POST['logname'];
+				echo htmlspecialchars($_POST['logname']);
 			} ?>"></p>
 			<p>パスワード<span><?php if(isset($error_password)){ echo $error_password;} ?></span></p>
 			<p><input type="password" name="password" value="<?php if(isset($_POST['password'])){
-				echo $_POST['password'];
+				echo htmlspecialchars($_POST['password']);
 			} ?>"></p>
 			
 			<p><input type="submit" value="ログイン"></p>
+			<p><span class="left"><?php  if(isset($unapproved)){ echo $unapproved;}?></span></p>
 		</form>
 	</div>
 	<div class="new-entry">
 		<form action="" method="post">
 			<p>お名前 <span><?php if(isset($error_name)){ echo $error_name;} ?><?php if(isset($error)){ echo $error;} ?></span></p>
-			<p><input type="text" name="login-name"  value="<?php if(isset($_POST['login-name'])){echo $_POST['login-name'];} ?>"></p>
+			<p><input type="text" name="login-name"  value="<?php if(isset($_POST['login-name'])){echo htmlspecialchars($_POST['login-name']);} ?>"></p>
 			<p>メールアドレス<span><?php if(isset($error_mail)){ echo $error_mail;} ?></span></p>
-			<p><input type="text" name="mail" value="<?php if(isset($_POST['mail'])){echo $_POST['mail'];} ?>"></p>
+			<p><input type="text" name="mail" value="<?php if(isset($_POST['mail'])){echo htmlspecialchars($_POST['mail']);} ?>"></p>
 			<p>パスワード<span><?php if(isset($error_passwords)){ echo $error_passwords;} ?></span></p>
-			<p><input type="password" name="passwords" <?php if(isset($_POST['passwords'])){echo $_POST['passwords'];} ?>></p>
+			<p><input type="password" name="passwords" <?php if(isset($_POST['passwords'])){echo htmlspecialchars($_POST['passwords']);} ?>></p>
 			
 			<p><input type="submit" value="登録"></p>
 		</form>
